8395062909b45e1396df4f6d3602180bf8b53f98
bperalta84 <bars_cp@yahoo.com>
2016-09-25 14:38:47 +0800

    removed unused import

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,10 +1,7 @@
 import {RouterModule} from "@angular/router";
-import {HomeComponent} from "./home/home.component";
 const routes =[
     {path:'', loadChildren:'app/home/home.module'},
     {path:'articles', loadChildren:'app/article/articles.module'}
-
-
 ];
 
 export default RouterModule.forRoot(routes);
