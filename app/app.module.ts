7f4805b66fa01c4b12ee6e56c7ef0a9e2f30ce46
bperalta84 <bars_cp@yahoo.com>
2016-09-27 22:54:51 +0800

    added location strategy
    Issue# get error Cannot GET /articles when page reload
    Solution# added location strategy HashLocation strategy, though this
    made the url use #.
    http://localhost/#/articles

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from "./app.component";
 import {BrowserModule} from "@angular/platform-browser";
 import appRoutes from "./app.routes"
+import {LocationStrategy, HashLocationStrategy} from "@angular/common";
 @NgModule({
     declarations: [AppComponent],
     imports: [ BrowserModule, appRoutes],
-    bootstrap: [ AppComponent ]
+    bootstrap: [ AppComponent ],
+    providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy}]
 })
 export class AppModule {
 
