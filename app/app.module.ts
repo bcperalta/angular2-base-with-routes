6084f42e9a7bb54626d442c98d3f297df4b9b853
bperalta84 <bars_cp@yahoo.com>
2016-10-02 14:41:44 +0800

    remove hash in url
    remove hash in url, removed HashLocationStrategy and modified
    live-server entry file to point to index.html

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,10 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from "./app.component";
 import {BrowserModule} from "@angular/platform-browser";
 import appRoutes from "./app.routes"
-import {LocationStrategy, HashLocationStrategy} from "@angular/common";
 @NgModule({
     declarations: [AppComponent],
     imports: [ BrowserModule, appRoutes],
-    bootstrap: [ AppComponent ],
-    providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy}]
+    bootstrap: [ AppComponent ]
 })
 export class AppModule {
 
